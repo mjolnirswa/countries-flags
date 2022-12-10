aa1baf5f30790cace8f6ebe2fe8c53a3cc235c77
Matvey Melnik <114065111+mjolnirswa@users.noreply.github.com>
2022-12-10 19:21:31 +0300

    second commit

---
+++ src/features/countries/use-countries.js
@@ -0,0 +1,19 @@
+import { selectControls } from "../controls/controls-slice";
+import { loadCountries, selectCountriesInfo, selectVisibleCountries } from "./countries-slice";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export const useCountries = () => {
+    const dispatch = useDispatch();
+    const controls = useSelector(selectControls);
+    const countries = useSelector(state => selectVisibleCountries(state, controls));
+    const {status, error, qty} = useSelector(selectCountriesInfo);
+  
+    useEffect(() => {
+      if (!qty) {
+        dispatch(loadCountries());
+      }
+    }, [qty, dispatch]);
+
+    return [countries, {status, error, qty}];
+}
